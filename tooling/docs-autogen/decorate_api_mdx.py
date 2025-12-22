d5f9f6b5fafce35e7ab0355399ae3c805919abb1
Mishal Dholakia <mishal.dholakia1@ibm.com>
2025-12-22 11:14:41 -0500

    chore: Try new hr fix

--- tooling/docs-autogen/decorate_api_mdx.py
+++ tooling/docs-autogen/decorate_api_mdx.py
@@ -87,7 +87,8 @@ FUNC_SPAN = (
 
 SPAN_RE = re.compile(r'\s*<span className="[^"]*rounded-full[^"]*">.*?</span>\s*')
 LABEL_RE = re.compile(r'^\[(class|func|Class|funct)\]\s+')
-DIVIDER_LINE = '---'
+# DIVIDER_LINE = '---'
+DIVIDER_LINE = '<div className="w-full h-px bg-gray-200 dark:bg-gray-700 my-4" />'
 
 
 def pick_kind(name: str, level: int, current_section: str | None) -> str | None:
